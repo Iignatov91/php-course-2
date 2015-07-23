393c0f7416b250a9d96fffde6990deaff8734a30
ivan <ivan.ignatov91@gmail.com>
2015-07-24 00:43:18 +0300

    praktice-php

---
+++ 04-php-basics-1/index.php
@@ -0,0 +1,22 @@
+<?php
+$My_name="Ivan";
+$Second_name="Ignatov";
+echo $My_name . $Second_name;
+echo "<br />";
+
+$Day_Of_Month = 23;
+	if( 25 < $Day_Of_Month || $Day_Of_Month == 23)
+	{
+		echo "Payment_is_near 23 ";
+	} else {
+		echo "There_is_no_payment 25 ";
+	}
+echo "<br />";
+
+$a = 9;
+
+$b = 11;
+
+$c = 15;
+
+echo $a;
